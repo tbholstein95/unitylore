6dd597ee0ebd53b84d52315755c59ccd624338a0
tbholstein95 <60832007+tbholstein95@users.noreply.github.com>
2020-08-24 15:18:04 -0400

    Add menu button
    Able to click Build menu and see an image. Can click the button again to collapse.

---
+++ Lore/Assets/Scripts/BuildMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildMenu : MonoBehaviour
+{
+    public GameObject buildMenuUI;
+
+    private void Start()
+    {
+        buildMenuUI.SetActive(false);
+    }
+
+    public void test()
+    {
+        Debug.Log("I will open the menu!");
+        buildMenuUI.SetActive(!buildMenuUI.activeSelf);
+    }
+}
