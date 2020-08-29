6275c440c4c242cbf8a2a173047a1419cddaaa5a
tbholstein95 <60832007+tbholstein95@users.noreply.github.com>
2020-08-28 20:40:45 -0400

    Add JobBoard sprite and canvas
    title

---
+++ Lore/Assets/Scripts/HireBoard.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HireBoard : MonoBehaviour
+{
+    bool clicking = false;
+
+    public GameObject HireBoardUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HireBoardUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggle()
+    {
+        Debug.Log("I turned the hire board on!");
+        HireBoardUI.SetActive(!HireBoardUI.activeSelf);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            clicking = true;
+
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            toggle();
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        if(clicking == true)
+        {
+            toggle();
+        }
+    }
+
+
+}
